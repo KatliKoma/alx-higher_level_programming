8e99a8e1a6a10b0fb5b5263ac1b741a16e2d6800
KatliKoma <katlegokoma1@gmail.com>
2024-01-04 14:16:20 +0200

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
-    args = sys.argv[1:]
-    
-    # Convert arguments to integers and sum them
-    result = sum(map(int, args))
-    
-    print(result)
+    sum = 0
+    for i in range(1, len(sys.argv)):
+        sum = sum + int(sys.argv[i])
+    print(sum)
