ca53fe0ed83c4361be65ded56624da2189362d82
KatliKoma <katlegokoma1@gmail.com>
2024-01-22 21:40:20 +0200

    Raise exception

--- 0x05-python-exceptions/5-raise_exception.py
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
 def raise_exception():
-    raise TypeError("Exception raised")
-
-# Example usage
-try:
-    raise_exception()
-except TypeError as te:
-    print(te)
+    raise TypeError("Exception raised"
