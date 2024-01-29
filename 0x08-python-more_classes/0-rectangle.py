2df420b7e0de5a37a3e29d47dc892bb56c1c1c55
KatliKoma <katlegokoma1@gmail.com>
2024-01-29 18:47:07 +0200

    Simple rectangle

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+Module for class Rectangle
+"""
+
+
+class Rectangle:
+    """Class Rectangle"""
+
+    def __init__(self):
+        """Initialized class"""
+        pass
