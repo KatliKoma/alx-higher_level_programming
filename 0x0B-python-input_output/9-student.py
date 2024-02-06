78987cedb568643e64fb8a5bca1a5b8ec222adfd
KatliKoma <katlegokoma1@gmail.com>
2024-02-06 15:26:59 +0200

    Student to JSON

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,28 +1,13 @@
 #!/usr/bin/python3
-"""Module 9-student.
-Introduces the Student class for creating student objects.
+""" Module designed to generate a dictionary representation 
+    using simple data structures, facilitating the JSON serialization of an object.
 """
 
-class Student:
-    """Defines a student with attributes and methods.
-    Attributes:
-        - first_name: First name of the student.
-        - last_name: Last name of the student.
-        - age: Age of the student.
-    Methods:
-        - to_json: Returns a dictionary representation of the student.
-    """
 
-    def __init__(self, first_name, last_name, age):
-        """Constructs a new Student object with the given attributes."""
+def class_to_json(obj):
+    """ Function that retuns the dictionary description of an obj """
 
-        self.first_name = first_name
-        self.last_name = last_name
-        self.age = age
-
-    def to_json(self):
-        """Converts the student object to a dictionary format.
-        Returns: A dictionary containing the student's attributes.
-        """
-
-        return self.__dict__
+    res = {}
+    if hasattr(obj, "__dict__"):
+        res = obj.__dict__.copy()
+    return res
