8cd0b55289ed93c278e5dfa6719249edce55a279
KatliKoma <katlegokoma1@gmail.com>
2024-02-06 13:03:58 +0200

    To JSON string

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"""Provides a function to convert objects to their JSON string representation."""
+"""string-to-JSON function."""
 import json
 
-def to_json_string(my_obj):
-    """Converts and returns an object's JSON string representation.
 
-    Parameters:
-        my_obj: The object to be converted into JSON format.
-    Returns:
-        str: A JSON-formatted string representation of the object.
-    """
+def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
     return json.dumps(my_obj)
