d113b85d102232b7fc6c7598c577a79dc9ae3262
KatliKoma <katlegokoma1@gmail.com>
2024-02-12 10:44:03 +0200

    Validate attributes

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,15 +6,7 @@ class Rectangle(Base):
     """Rectangle class that inherits from Base."""
 
     def __init__(self, width, height, x=0, y=0, id=None):
-        """Initialize a new Rectangle.
-
-        Args:
-            width (int): The width of the rectangle.
-            height (int): The height of the rectangle.
-            x (int): The x coordinate of the rectangle.
-            y (int): The y coordinate of the rectangle.
-            id (int): The id of the rectangle.
-        """
+        """Initialize a new Rectangle."""
         super().__init__(id)
         self.width = width
         self.height = height
@@ -29,6 +21,7 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Set the width of the rectangle."""
+        self.validate("width", value, False)
         self.__width = value
 
     @property
@@ -39,6 +32,7 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Set the height of the rectangle."""
+        self.validate("height", value, False)
         self.__height = value
 
     @property
@@ -49,6 +43,7 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Set the x coordinate of the rectangle."""
+        self.validate("x", value, True)
         self.__x = value
 
     @property
@@ -59,4 +54,15 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Set the y coordinate of the rectangle."""
+        self.validate("y", value, True)
         self.__y = value
+
+    @staticmethod
+    def validate(attribute, value, non_negative):
+        """Validate an attribute with its value."""
+        if type(value) is not int:
+            raise TypeError(f"{attribute} must be an integer")
+        if non_negative and value < 0:
+            raise ValueError(f"{attribute} must be >= 0")
+        if not non_negative and value <= 0:
+            raise ValueError(f"{attribute} must be > 0")
