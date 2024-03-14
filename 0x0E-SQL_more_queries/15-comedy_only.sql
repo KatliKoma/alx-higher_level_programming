4ef8c0568e36d42d20bb97a08caafec1aefda8db
KatliKoma <katlegokoma1@gmail.com>
2024-03-14 04:22:28 +0200

    Only Comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,3 +1,8 @@
+-- Retrieves all Comedy shows from the database hbtn_0d_tvshows.
+-- Output format for each record:
+-- tv_shows.title
+-- Organize the output in ascending order by the show title.
+-- The name of the database will be provided as a parameter to the MySQL command.
 SELECT tv_shows.title
 FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
