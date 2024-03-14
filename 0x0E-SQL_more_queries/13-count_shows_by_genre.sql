7dfa929eb7c2dbdbe24c78485a9c2018a1ba8721
KatliKoma <katlegokoma1@gmail.com>
2024-03-14 04:19:15 +0200

    Number of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,8 @@
+-- Retrieves all genres from the database hbtn_0d_ and counts how many shows are associated with each genre.
+-- Output format for each record:
+-- Genre of the TV show - Total number of shows associated with this genre
+-- Organize the output in descending order based on the count of shows linked to each genre.
+-- The name of the database will be provided as a parameter to the MySQL command.
 SELECT genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows
 FROM genres
 JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
