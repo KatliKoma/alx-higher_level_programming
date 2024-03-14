7a5bd2ce1a3c14564409abe9d02773d015816668
KatliKoma <katlegokoma1@gmail.com>
2024-03-14 03:59:48 +0200

    name

--- 0x0E-SQL_more_queries/3-force_name.sql
+++ 0x0E-SQL_more_queries/3-force_name.sql
@@ -1,4 +1,2 @@
-CREATE TABLE IF NOT EXISTS force_name (
-  id INT,
-  name VARCHAR(256) NOT NULL
-);
+-- creates the table force_name
+CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
