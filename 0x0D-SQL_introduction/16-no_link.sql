a4a61c3357194aae0fa36d40124d01c09a09a385
KatliKoma <katlegokoma1@gmail.com>
2024-03-14 23:40:20 +0200

    Say my name

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,2 @@
+-- This script lists all records from second_table with a non-empty name, ordered by descending score
+SELECT score, name FROM second_table WHERE name IS NOT NULL AND name <> '' ORDER BY score DESC;
