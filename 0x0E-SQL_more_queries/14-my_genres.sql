64abb516ede734e897ac35b4d35ed6eb6c7ff890
KatliKoma <katlegokoma1@gmail.com>
2024-03-14 04:21:01 +0200

    My genres

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,3 +1,8 @@
+-- Retrieves all genres associated with the show Dexter.
+-- Output format for each record:
+-- tv_genres.name
+-- Organize the output in ascending order by the genre name.
+-- The name of the database will be provided as a parameter to the MySQL command.
 SELECT genres.name
 FROM genres
 JOIN tv_show_genres ON genres.id = tv_show_genres.genre_id
