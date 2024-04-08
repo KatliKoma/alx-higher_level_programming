509f6ea121e5a3379c43fd24114478b50e0b0271
KatliKoma <katlegokoma1@gmail.com>
2024-04-08 21:46:55 +0200

    add file

--- 0x12-javascript-warm_up/13-add.js
+++ 0x12-javascript-warm_up/13-add.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 
-if (process.argv.length <= 3) {
-  console.log(0);
-} else {
-  const list = process.argv.sort();
-  console.log(list.reverse()[1]);
+function add(a, b) {
+  return a + b;
 }
+
+module.exports.add = add;
