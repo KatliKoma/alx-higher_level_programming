970883e5cec68ef3b3f2a205c147dee395eb8b8e
KatliKoma <katlegokoma1@gmail.com>
2024-04-09 23:14:49 +0200

    Esrever

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// File: 8-esrever.js
+
+exports.esrever = function (list) {
+  let reversedList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return reversedList;
+};
