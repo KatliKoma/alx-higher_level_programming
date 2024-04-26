749d95b950f506cc68d67226f2eba100426fb9f8
KatliKoma <katlegokoma1@gmail.com>
2024-04-26 13:43:06 +0200

    Response header value

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 import urllib.request
 import sys
+"""
+This script fetches the 'X-Request-Id' header value from a URL provided as a command-line argument using urllib.request.
+"""
 
 if __name__ == "__main__":
     url = sys.argv[1]
