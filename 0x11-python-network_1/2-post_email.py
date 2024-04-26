39a62424535966c1310116d444510ea1082a17e6
KatliKoma <katlegokoma1@gmail.com>
2024-04-26 13:45:11 +0200

    POST an email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+
+"""
+This script sends a POST request to a URL with an email parameter.
+"""
+
 import urllib.parse
 import urllib.request
 import sys
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    email = sys.argv[2]
-
-    data = urllib.parse.urlencode({'email': email})
-    data = data.encode('ascii')
+    url, email = sys.argv[1], sys.argv[2]
+    data = urllib.parse.urlencode({'email': email}).encode('ascii')
 
     with urllib.request.urlopen(url, data) as response:
         print(response.read().decode('utf-8'))
+
