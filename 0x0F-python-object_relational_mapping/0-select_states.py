ac72a684d9996fdcc61ea3ceac9ddfbedba7b1fa
KatliKoma <katlegokoma1@gmail.com>
2024-04-29 11:41:05 +0200

    Get all states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,11 @@
 #!/usr/bin/python3
 """Lists all states from the database hbtn_0e_0_usa"""
 
-import MySQLdb
 import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    # Check if correct number of arguments is provided
-    if len(sys.argv) != 4:
-        print("Usage: ./0-select_states.py <username> <password> <database>")
-        sys.exit(1)
-
-    # Connect to MySQL database
-    db = MySQLdb.connect(host="localhost", port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-
-    # Create cursor
-    cur = db.cursor()
-
-    # Execute SQL query to select all states
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Initialize a set to store distinct states
-    distinct_states = set()
-
-    # Fetch all rows and print distinct states
-    for row in cur.fetchall():
-        state_id, state_name = row
-        if state_name not in distinct_states:
-            print(row)
-            distinct_states.add(state_name)
-
-    # Close cursor and database connection
-    cur.close()
-    db.close()
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM `states`")
+    [print(state) for state in c.fetchall()]
