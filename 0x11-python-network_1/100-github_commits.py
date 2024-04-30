26d7e544463f553661f7bb22a6e6ce7dd090f952
KatliKoma <katlegokoma1@gmail.com>
2024-05-01 00:33:44 +0200

    Time for an interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python script that retrieves the 10 most recent commits
+(from most recent to oldest) of the repository “rails” by the user “rails”."""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/{}/{}/commits?per_page=10"\
+          .format(argv[2], argv[1])
+
+    response = requests.get(url)
+    commits = response.json()
+    for commit in commits:
+        print("{}: {}".format(commit.get("sha"),
+                              commit.get("commit").get("author").get("name")))
+
