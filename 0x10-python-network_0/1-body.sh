8c7f1c17b6a8534d5a789dc465a666c5a04f7510
KatliKoma <katlegokoma1@gmail.com>
2024-04-30 23:57:43 +0200

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-
-# Send GET request with silent mode and capture response
-response=$(curl -s "$1")
-
-# Check status code and display body if 200
-if [[ $(echo "$response" | head -n 1) -eq 200 ]]; then
-  echo "${response#*  }"  # Remove leading status code
-fi
+# Displays the body of the response of a curl request
+curl -sL "$1"
