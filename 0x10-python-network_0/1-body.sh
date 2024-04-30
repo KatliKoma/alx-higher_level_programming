64f8fb07c19956684e0bdd6559a1a700143e4488
KatliKoma <katlegokoma1@gmail.com>
2024-04-30 23:47:39 +0200

    cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#This Bash script takes a URL as input, sends a request to it, and shows the size of the response body using curl.
-curl -sX GET $1 -L
+# shellcheck disable=SC2046
+if [ $(curl -L -s -X HEAD -w "%{http_code}" "$1") == '200' ]; then curl -Ls "$1"; fi
