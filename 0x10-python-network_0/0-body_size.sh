0f08a2526e53a37e212611e0fbaf2b66256e0f2f
KatliKoma <katlegokoma1@gmail.com>
2024-04-30 23:38:39 +0200

    cURL body size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-# Check if URL argument is provided
-if [ $# -eq 0 ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
-
-# Send request to URL and store response in a temporary file
-response=$(curl -s -w "%{size_download}" -o /dev/null "$1")
-
-# Display the size of the body of the response
-echo "$response"
-
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
